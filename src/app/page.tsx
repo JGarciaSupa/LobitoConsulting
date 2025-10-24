b005298feb33644b3155fcb8ae1edbadbe011840
Renato Navarro A. <72818352+devrenatonavarro@users.noreply.github.com>
2025-10-24 08:38:29 -0500

    init page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-    <h1>Que rico aprietas Juan Garcia Supa</h1>
+    <div className="w-full min-h-dvh flex flex-col items-center justify-center bg-[#D78131] p-4">
+      <img className="w-full max-w-[280px]" src="/logo.webp" alt="Logo" />
+      <h1 className="text-white text-4xl font-light italic mt-4">Lobito Consulting</h1>
+    </div>
   );
 }
