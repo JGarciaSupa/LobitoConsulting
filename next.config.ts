6ae5d031edb08d4f4ebb54adc09f09eb88dd8edb
Renato Navarro A. <72818352+devrenatonavarro@users.noreply.github.com>
2025-12-15 18:53:09 -0500

    configuracion de tiktok

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options heres */ 
+  /* config options heres lobito tikitiki */ 
 };
 
 export default nextConfig;
